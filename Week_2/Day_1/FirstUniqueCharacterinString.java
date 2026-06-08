cf76e75f85e10308755d9907430268770e16e757
Aman Sahu <amansahuking223@gmail.com>
2026-06-08 20:07:31 +0530

    Problem 17

---
+++ Week_2/Day_1/FirstUniqueCharacterinString.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        int freq[] = new int[26];
+      
+        Queue<Character> q = new LinkedList<>();
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            q.add(ch);
+            freq[ch-'a']++;
+
+            while(!q.isEmpty() && freq[q.peek()-'a']>1){
+                q.remove();
+            }
+        }
+        if(q.isEmpty()){
+                return -1;
+        }
+        char firstUniq = q.peek();
+
+        for(int i= 0;i<s.length();i++){
+           if(s.charAt(i)==firstUniq){
+            return i;
+           }
+
+           
+
+        }
+        return -1; 
+
+        
+    }
+}
