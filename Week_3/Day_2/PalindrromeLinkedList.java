72c505f270eb7608359504d04f7de3ef25e794b4
Aman Sahu <amansahuking223@gmail.com>
2026-06-18 10:26:30 +0530

    Problem 36

---
+++ Week_3/Day_2/PalindrromeLinkedList.java
@@ -0,0 +1,42 @@
+class Solution {
+    public ListNode findMid(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    public boolean isPalindrome(ListNode head) {
+        if(head == null || head.next == null){
+            return true;
+        }
+
+        ListNode mid = findMid(head);
+
+        ListNode prev = null;
+        ListNode curr = mid;
+        ListNode next;
+
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        ListNode right = prev;
+        ListNode left = head;
+
+        while(right != null){
+            if(left.val != right.val){
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+            
+        }
+        return true;
+    }
+}
