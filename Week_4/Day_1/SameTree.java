415cb5be473286bc13bd9b7ae7bc2a88c9b00220
Aman Sahu <amansahuking223@gmail.com>
2026-06-27 21:03:48 +0530

    solve

---
+++ Week_4/Day_1/SameTree.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null){
+            return true;
+        } 
+        else if(p == null || q == null || p.val != q.val){
+            return false;
+        }
+        if(!isSameTree(p.left , q.left)){
+            return false;
+        }
+        if(!isSameTree(p.right , q.right)){
+            return false;
+        }
+        return true;
+        
+    }
+}
